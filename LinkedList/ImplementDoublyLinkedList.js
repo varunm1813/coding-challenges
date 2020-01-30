7206c52419e4580b526ab07a18bbc060c8fdce4f
varun mara <varungowde@gmail.com>
2020-01-30 03:44:40 -0600

    implemented doubly linkedlist

---
+++ LinkedList/ImplementDoublyLinkedList.js
@@ -0,0 +1,106 @@
+// add a method remove() to the linked list that deletes a node to the specified index.
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+      prev: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    };
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    };
+    this.head.prev = newNode;
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    //Check for proper parameters;
+    if (index >= this.length) {
+      console.log("yes");
+      return this.append(value);
+    }
+
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    };
+    const leader = this.traverseToIndex(index - 1);
+    newNode.next = leader.next;
+    const holdingPointer = leader.next;
+    newNode.prev = holdingPointer.prev;
+    holdingPointer.prev = newNode;
+    leader.next = newNode;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    // Check Parameters
+    if (index === 0) {
+      this.head = this.head.next;
+      this.head.prev = null;
+      return;
+    }
+
+    const leader = this.traverseToIndex(index - 1);
+    if (index === this.length - 1) {
+      leader.next = null;
+      return;
+    }
+    const unwantedNode = leader.next;
+    const nextNode = unwantedNode.next;
+    nextNode.prev = unwantedNode.prev;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+myLinkedList.remove(5);
+myLinkedList.printList();
